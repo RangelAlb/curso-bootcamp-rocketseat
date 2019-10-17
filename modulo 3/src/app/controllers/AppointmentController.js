4ea914d9ce2e6ed39f9da7d2f3211cdb6c70f8ec
Rangel Albuquerque <rangel_albuquerque_2012@hotmail.com>
2019-10-17 09:27:23 -0300

    Modulo 03 - Aula 5 e 6

---
+++ modulo 3/src/app/controllers/AppointmentController.js
@@ -0,0 +1,64 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO } from 'date-fns';
+import { isBefore } from 'date-fns/esm';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validação Falhou' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /* Checando se o provider_id é um provider comum
+     */
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Você só pode criar appointments com providers' });
+    }
+
+    /* Checando data passada */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Datas passadas não permitidas' });
+    }
+
+    /* Checando data disponivel  */
+
+    const checkDisponivel = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkDisponivel) {
+      return res.status(400).json({ error: 'Data não está disponível' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
