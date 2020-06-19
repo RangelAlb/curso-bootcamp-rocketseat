7b412a90f7580bd55d3688b11d4d09b11c1d1f3a
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2020-06-19 08:02:48 -0300

    Modulo 06 - Aula 10: Acessando a API do Git

--- modulo 6/src/pages/Main/index.js
+++ modulo 6/src/pages/Main/index.js
@@ -1,27 +1,60 @@
 /* eslint-disable react/jsx-no-undef */
-import React from 'react';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+export default class Main extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    newUser: '',
+    users: [],
+  };
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeHolder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="Add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleaddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeHolder="Adicionar usuário"
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleaddUser}
+          />
+          <SubmitButton onPress={this.handleaddUser}>
+            <Icon name="Add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
 Main.navigationOptions = {
   title: 'Usuários',
 };
