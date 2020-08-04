df7d37fc1e8d470f47b20fbe47c84d02bea2113d
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2020-08-04 07:59:14 -0300

    Modulo 07 - Aula 21: Reactotron + Saga

--- modulo 7/src/config/ReactotronConfig.js
+++ modulo 7/src/config/ReactotronConfig.js
@@ -1,8 +1,12 @@
 import Reacttron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reacttronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reacttron.configure().use(reactotronRedux()).connect();
+  const tron = Reacttron.configure()
+    .use(reactotronRedux())
+    .use(reacttronSaga())
+    .connect();
 
   tron.clear();
 
